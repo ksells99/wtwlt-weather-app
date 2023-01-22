9c27851d3e16f283c1e497cf756f41e54d181c66
Kai Sells <kai.sells@hotmail.co.uk>
2023-01-22 14:19:30 +0000

    Another oops

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
