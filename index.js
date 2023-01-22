97e17e760ebf5f6623fbfdae866c7767c3c62af3
Kai Sells <kai.sells@hotmail.co.uk>
2023-01-22 14:38:45 +0000

    Another oops

--- index.js
+++ index.js
@@ -29,7 +29,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        "client",
+        "dist",
+        "wtwlt-weather-app",
+        "index.html"
+      )
+    );
   });
 }
 
